3a2e8e7b39bec951eb6b456923742af214668afa
piyo <chick.p.1120@gmail.com>
2022-06-11 15:47:24 +0900

    feat: check period with part of speech

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import { TextlintRuleModule } from "@textlint/types";
 // @ts-ignore
 import checkEndsWithPeriod from "check-ends-with-period";
+import { tokenize } from "kuromojin";
 
 export interface Options {
   periodMarks: string[];
   preferPeriodMark: string;
+  allowPosWithoutPeriod: string[];
 }
 
 const defaultOptions: Options = {
   periodMarks: ["。", "."],
   preferPeriodMark: "。",
+  allowPosWithoutPeriod: ["名詞", "記号"],
 };
+
 // @ts-ignore
 const report: TextlintRuleModule<Options> = (context, options = {}) => {
-  const { Syntax, RuleError, report, fixer, getSource } = context;
+  const { Syntax, RuleError, report, getSource } = context;
   const periodMarks = options.periodMarks || defaultOptions.periodMarks;
   const preferPeriodMark =
     options.preferPeriodMark || defaultOptions.preferPeriodMark;
+  const allowPosWithoutPeriod =
+    options.allowPosWithoutPeriod || defaultOptions.allowPosWithoutPeriod;
   return {
     async [Syntax.ListItem](node) {
       const paragraphNodes = node.children.filter(
@@ -24,31 +30,40 @@ const report: TextlintRuleModule<Options> = (context, options = {}) => {
       );
       const [firstParagraphNode] = paragraphNodes;
       const text = getSource(firstParagraphNode);
-      const { valid, index } = checkEndsWithPeriod(text, {
+
+      const { valid: hasPeriod } = checkEndsWithPeriod(text, {
         periodMarks,
+        allowExceptionMark: false,
       });
-      if (valid) {
+      const tokens = await tokenize(text);
+      if (hasPeriod) {
+        // if a sentense has a period though disallow part of speech
+        const lastToken = tokens.at(-2);
+        if (lastToken && allowPosWithoutPeriod.includes(lastToken.pos)) {
+          report(
+            firstParagraphNode,
+            new RuleError(
+              `Shoud remove period mark("${preferPeriodMark}") at end of list item.`
+            )
+          );
+          return;
+        }
         return;
       }
-      report(
-        firstParagraphNode,
-        new RuleError(
-          `Not exist period mark("${preferPeriodMark}") at end of list item.`,
-          {
-            index,
-            fix: fixer.replaceTextRange(
-              [index + 1, index + 1],
-              preferPeriodMark
-            ),
-          }
-        )
-      );
+      // if a sentence has not a period though without period
+      const lastToken = tokens.at(-1);
+      if (lastToken && !allowPosWithoutPeriod.includes(lastToken.pos)) {
+        report(
+          firstParagraphNode,
+          new RuleError(
+            `Not exist period mark("${preferPeriodMark}") at end of list item.`
+          )
+        );
+        return;
+      }
       return;
     },
   };
 };
 
-export default {
-  linter: report,
-  fixer: report,
-};
+export default report;
