9d2ffad10f69d1d9a1dbb01853cd464ad1fc00b5
piyo <chick.p.1120@gmail.com>
2022-06-12 22:39:39 +0900

    feat: add `isAppendPeriod` option

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,24 @@ import { tokenize } from "kuromojin";
 
 export interface Options {
   periodMarks: string[];
+  isAppendPeriod: boolean;
   preferPeriodMark: string;
   allowPosWithoutPeriod: string[];
 }
 
 const defaultOptions: Options = {
   periodMarks: ["。", "."],
+  isAppendPeriod: false,
   preferPeriodMark: "。",
   allowPosWithoutPeriod: ["名詞", "記号"],
 };
 
 // @ts-ignore
 const report: TextlintRuleModule<Options> = (context, options = {}) => {
-  const { Syntax, RuleError, report, getSource } = context;
+  const { Syntax, RuleError, report, fixer, getSource } = context;
   const periodMarks = options.periodMarks || defaultOptions.periodMarks;
+  const isAppendPeriod =
+    options.isAppendPeriod || defaultOptions.isAppendPeriod;
   const preferPeriodMark =
     options.preferPeriodMark || defaultOptions.preferPeriodMark;
   const allowPosWithoutPeriod =
@@ -31,7 +35,7 @@ const report: TextlintRuleModule<Options> = (context, options = {}) => {
       const [firstParagraphNode] = paragraphNodes;
       const text = getSource(firstParagraphNode);
 
-      const { valid: hasPeriod } = checkEndsWithPeriod(text, {
+      const { valid: hasPeriod, index } = checkEndsWithPeriod(text, {
         periodMarks,
         allowExceptionMark: false,
       });
@@ -53,10 +57,21 @@ const report: TextlintRuleModule<Options> = (context, options = {}) => {
       // if a sentence has not a period though without period
       const lastToken = tokens.at(-1);
       if (lastToken && !allowPosWithoutPeriod.includes(lastToken.pos)) {
+        let fix;
+        if (isAppendPeriod) {
+          fix = fixer.replaceTextRange(
+            [index + 1, index + 1],
+            preferPeriodMark
+          );
+        }
         report(
           firstParagraphNode,
           new RuleError(
-            `Not exist period mark("${preferPeriodMark}") at end of list item.`
+            `Not exist period mark("${preferPeriodMark}") at end of list item.`,
+            {
+              index,
+              fix,
+            }
           )
         );
         return;
@@ -66,4 +81,7 @@ const report: TextlintRuleModule<Options> = (context, options = {}) => {
   };
 };
 
-export default report;
+export default {
+  linter: report,
+  fixer: report,
+};
